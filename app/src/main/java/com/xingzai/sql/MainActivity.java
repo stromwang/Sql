c38da1572d4563cb1f9480297570b97a59b472c5
stromwang <739075507@qq.com>
2017-04-29 20:52:04 +0800

    Initial commit

---
+++ app/src/main/java/com/xingzai/sql/MainActivity.java
@@ -0,0 +1,25 @@
+package com.xingzai.sql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper=new MyDatabaseHelper(this,"Bookstore.db",null,1);
+        Button createDatabase=(Button)findViewById(R.id.create_data);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+    }
+}
